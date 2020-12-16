5e16f5dbbcd81ba43bc762d9f5fb2f6719b7395a
jfchen19 <jchenf19@gmail.com>
2020-12-16 15:06:46 +0800

    add name column

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_paramseters, if: :devise_controller?
+
+  private
+  def configure_permitted_paramseters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
+    devise_parameter_sanitizer.permit(:account_update, keys:[:name])
+  end
 end
